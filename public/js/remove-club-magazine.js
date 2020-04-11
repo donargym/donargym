0dae40f477c1f23f686dcda7d26e8d6989e6d398
Mark Meijerman <mark@future500.nl>
2020-04-11 20:50:11 +0200

    remove debug

--- public/js/remove-club-magazine.js
+++ public/js/remove-club-magazine.js
@@ -2,7 +2,6 @@ $('.remove-club-magazine').click(function () {
     let date = $(this).data('date');
     let id = $(this).data('id');
     let modal = $('#remove-club-magazine-modal');
-    console.log(modal);
     modal.find('.modal-body #item-date').text(date);
     modal.find('.modal-body #id').val(id);
     modal.modal('show');
